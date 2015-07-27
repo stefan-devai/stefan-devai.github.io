2d0318b40b01b40075a82b388c31b40c12c5d610
Stefan Devai <stefan.devai47@gmail.com>
2015-07-27 04:41:22 -0700

    removed fade-in and fade-out

--- js/main-scripts.js
+++ js/main-scripts.js
@@ -1,22 +1,10 @@
 $(document).ready(function() {
-	$(".wrapper").css("display", "none");
-	$(".wrapper").fadeIn(300);
-
 	if(window.location.pathname != $(".site-title").attr('href')) {
 		$(".site-title").hover(inTitle, outTitle);
 	}
 	else {
 		$(".title-underline").css("width", "100%");
-	}
-
-	$(".fade-out").click(function(e){
-        redirect = $(this).attr('href');
-        e.preventDefault();
-        $(".wrapper").fadeOut(50, function(){
-            document.location.href = redirect;
-        });
-        
-    });   
+	} 
 });
 
 var inTitle = function() {
